b2057d03d2c07995756bc3ee3379c4ca081ffff8
thuzax <arthucruz@hotmail.com>
2018-11-19 10:05:10 -0200

    Tabela com dados em relacao a variacao de k

--- main.py
+++ main.py
@@ -3,47 +3,55 @@ from MatrizConfusao import MatrizConfusao
 from MatrizAcertos import MatrizAcertos
 from LeitorArquivo import LeitorArquivo, LeitorArquivoComTestes
 from KNN import KNN
-
+from prettytable import PrettyTable
 def main():
     matrizAcertos = MatrizAcertos(10, 2, 3, 9)
 
 
-    # print(matrizAcertos)
-    # print(calculadoraEstatisticas)
+    dados = LeitorArquivoComTestes("spambase/spambase.data", 0.01)
+    todasEstatisticas = {}
+    print("------------------------------------------")
+    for k in [1,3,5,7]:
+        knn = KNN(dados[0])
+        resultado_knn = knn.run(dados[1], k)
+        resultados_esperados = []
+        for dado in dados[1]:
+            resultados_esperados.append(dado[-1])
+        
+        print("Resultados obtidos:")
+        print(resultado_knn)
+        print("Resultados esperados:")
+        print(resultados_esperados)
+        matrizConfusao = MatrizConfusao(dados[2])
+        matrizConfusao.geraMatriz(resultado_knn, resultados_esperados)
 
-    # dados = LeitorArquivo("spambase/spambase.data")
-    # dados_iris = LeitorArquivo("irisbase/iris.data")
-    # print(dados)
-    # knn = KNN(dados_iris)
-    # teste_iris = [[6.3,3.4,5.6,2.4]]
-    # teste = [0,0,1.42,0,0.71,0.35,0,0.35,0,0.71,0,0.35,0,0,0,5.35,0,0,3.21,0,2.85,0,0.35,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.102,0,0.357,0,0,1.971,24,205]
-    # print(knn.run(teste_iris,3))
+        matConf = matrizConfusao.getMatriz()
 
-    dados = LeitorArquivoComTestes("spambase/spambase.data", 0.05)
-    knn = KNN(dados[0])
-    # print(dados[1])
-    resultado_knn = knn.run(dados[1], 3)
-    resultados_esperados = []
-    for dado in dados[1]:
-        resultados_esperados.append(dado[-1])
+        matrizAcertos.negativoNegativo = matConf["0"]["0"]
+        matrizAcertos.negativoPositivo = matConf["0"]["1"]
+        matrizAcertos.positivoNegativo = matConf["1"]["0"]
+        matrizAcertos.positivoPositivo = matConf["1"]["1"]
+
+        print(matrizAcertos)
+
+        calculadoraEstatisticas = CalculadoraEstatisticas(matrizAcertos)
+        print(calculadoraEstatisticas)
+
+        todasEstatisticas[k] = calculadoraEstatisticas
+        print("------------------------------------------")
+        
+    escreveTabelaTodasEstatisticas(todasEstatisticas)
+
+def escreveTabelaTodasEstatisticas(todasEstatisticas):
+    saida = PrettyTable()
+    header = ["", "Acuracia", "Recall", "Precisao", "F-Score"]
     
-    print(resultado_knn)
-    print(resultados_esperados)
-    matrizConfusao = MatrizConfusao(dados[2])
-    matrizConfusao.geraMatriz(resultado_knn, resultados_esperados)
+    saida.field_names = header
+    for k in todasEstatisticas:
+        saida.add_row([str(k), todasEstatisticas[k].accuracy, todasEstatisticas[k].truePositiveRate,
+                        todasEstatisticas[k].precisao, todasEstatisticas[k].fScore])
 
-    matConf = matrizConfusao.getMatriz()
-
-    matrizAcertos.negativoNegativo = matConf["0"]["0"]
-    matrizAcertos.negativoPositivo = matConf["0"]["1"]
-    matrizAcertos.positivoNegativo = matConf["1"]["0"]
-    matrizAcertos.positivoPositivo = matConf["1"]["1"]
-
-    print(matrizAcertos)
-
-    calculadoraEstatisticas = CalculadoraEstatisticas(matrizAcertos)
-
-    print(calculadoraEstatisticas)
+    print(saida)
 
 
     
