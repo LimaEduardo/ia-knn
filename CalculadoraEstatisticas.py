c9778a83b64e77b32e1cce4abe9765c830067471
limaeduardo <eduardolima.dev@gmail.com>
2018-11-19 11:00:49 -0200

    Conserta analises estatisticas

--- CalculadoraEstatisticas.py
+++ CalculadoraEstatisticas.py
@@ -1,38 +1,108 @@
 # Calcula as estatísticas com base em uma matriz de confusão
 
 class CalculadoraEstatisticas:
-    def __init__(self, matrizAcertos):
-        self.negativoNegativo = matrizAcertos.negativoNegativo
-        self.negativoPositivo = matrizAcertos.negativoPositivo
-        self.positivoNegativo = matrizAcertos.positivoNegativo
-        self.positivoPositivo = matrizAcertos.positivoPositivo
+    def __init__(self, matrizConfusao):
+        self.classes = matrizConfusao.getClasses()
+        self.matriz = matrizConfusao.getMatriz()
 
-        total = self.negativoNegativo + self.negativoPositivo + self.positivoNegativo + self.positivoPositivo
+        self.classesRatings = []
 
-        # Accuracy: Fracao total correta
-        self.accuracy = (self.negativoNegativo + self.positivoPositivo) / (total)
-        
-        # Recall: numero de predicoes positivas que eram realmente positivas
-        self.truePositiveRate = self.positivoPositivo / (self.positivoNegativo + self.positivoPositivo)
-        
-        self.falsePositiveRate = self.negativoPositivo / (self.negativoNegativo + self.negativoPositivo)
-        self.trueNegativeRate = self.negativoNegativo / (self.negativoNegativo + self.negativoPositivo)
-        self.falseNegativeRate = self.positivoNegativo / (self.positivoNegativo + self.positivoPositivo)
+        for classe in self.classes:
+            truePositive = self.calculateTruePositive(classe)
+            falseNegative = self.calculateFalseNegative(classe)
+            falsePositive = self.calculateFalsePositive(classe)
+            trueNegative = self.calculateTrueNegative(classe)
 
-        # Precisao: numero de acertos positivos em relacao a quantidade real de positivos
-        self.precisao = self.positivoPositivo / (self.negativoPositivo + self.positivoPositivo)
+            # Recall: numero de predicoes positivas que eram realmente positivas
+            try:
+                truePositiveRate = truePositive / (truePositive + falseNegative)
+            except ZeroDivisionError:
+                truePositiveRate = 0
 
-        # F-Score: Media harmonica entre precisao e recall
-        self.fScore = 2 * (self.truePositiveRate  * self.precisao) / (self.truePositiveRate + self.precisao)
+            try:
+                # Specificity: numero de predicoes negativas que eram realmente negativas
+                trueNegativeRate = trueNegative / (trueNegative + falsePositive)
+            except ZeroDivisionError:
+                trueNegativeRate = 0
+
+            total = truePositive + falseNegative + falsePositive + trueNegative
+            
+            # Accuracy: Fracao total correta
+            if trueNegative + truePositive != 0:
+                accuracy = (trueNegative + truePositive)/total
+            else:
+                accuracy = "N/A"
+
+
+            try:
+                # Precisao: numero de acertos positivos em relacao a quantidade real de positivos   
+                precision = truePositive / (truePositive + falsePositive)
+            except ZeroDivisionError:
+                precision = "N/A"
+            
+            fScore = "N/A"
+            try:
+                # F-Score: Media harmonica entre precisao e recall
+                if precision != "N/A":
+                    fScore = 2 * (truePositiveRate  * precision) / (truePositiveRate + precision)
+            except ZeroDivisionError:
+                fScore = "N/A"
+            
+            classRating = {'nome': classe, 'truePositive': truePositiveRate, 'trueNegative': trueNegativeRate, 'accuracy': accuracy, 'precision': precision, 'fScore': fScore}
+            self.classesRatings.append(classRating)
+    
+    # Taxa de acerto
+    def calculateTruePositive(self,classe):
+        return self.matriz[classe][classe]
+    
+    # Soma de todos os números na linha correspondente (exceto a própria classe)
+    def calculateFalseNegative(self, classe):
+        falseNegative = 0
+        for column in self.matriz:
+            if column != classe:
+                falseNegative += self.matriz[classe][column]
+        return falseNegative
+    
+    # Soma de todos os números na coluna correspondente (exceto a própria classe)
+    def calculateFalsePositive(self, classe):
+        falsePositive = 0
+        for row in self.matriz:
+            if row != classe:
+                falsePositive += self.matriz[row][classe]
+        return falsePositive
+    
+    # Soma de todos os elementos excluíndo os da linha e coluna da tal classe
+    def calculateTrueNegative(self, classe):
+        trueNegative = 0
+        for row in self.matriz:
+            for column in self.matriz[row]:
+                if row == classe or column == classe:
+                    continue
+                trueNegative += self.matriz[row][column]
+        return trueNegative
+    
+    def calculateOverralAccuracy(self):
+        diagonal = 0
+        total = 0
+        for row in self.matriz:
+            for column in self.matriz:
+                if row == column:
+                    diagonal += self.matriz[row][column]
+                total += self.matriz[row][column]
+        return diagonal/total
+                
 
 
     def __str__(self):
         saida = ""
-        saida += "Taxa de falso negativo: " + str(self.falseNegativeRate) + "\n"
-        saida += "Taxa de falso positivo: " + str(self.falsePositiveRate) + "\n"
-        saida += "Taxa de verdadeiro negativo: " + str(self.trueNegativeRate) + "\n"
-        saida += "Taxa de verdadeiro positivo (Recall): " + str(self.truePositiveRate) + "\n"
-        saida += "Acuracia: " + str(self.accuracy) + "\n"
-        saida += "Precisao: " + str(self.precisao) + "\n"
-        saida += "F-Score:  " + str(self.fScore) + "\n"
+        for classe in self.classesRatings:
+            saida += "************** Classe : " + str(classe['nome']) + " **************" + "\n"
+            # saida += "Taxa de falso negativo: " + str(classe['falseNegative']) + "\n"
+            # saida += "Taxa de falso positivo: " + str(classe['falsePositive']) + "\n"
+            saida += "Taxa de verdadeiro positivo (Recall): " + str(classe['truePositive']) + "\n"
+            saida += "Taxa de verdadeiro negativo (Specificity): " + str(classe['trueNegative']) + "\n"
+            saida += "Acuracia: " + str(classe['accuracy']) + "\n"
+            saida += "Precisao: " + str(classe['precision']) + "\n"
+            saida += "F-Score:  " + str(classe['fScore']) + "\n\n"
+        saida += "Overral Accuracy: " + str(self.calculateOverralAccuracy())
         return saida
--- main.py
+++ main.py
@@ -4,44 +4,26 @@ from MatrizAcertos import MatrizAcertos
 from LeitorArquivo import LeitorArquivo, LeitorArquivoComTestes
 from KNN import KNN
 
+import sys
+
 def main():
-    matrizAcertos = MatrizAcertos(10, 2, 3, 9)
-
-
-    # print(matrizAcertos)
-    # print(calculadoraEstatisticas)
-
-    # dados = LeitorArquivo("spambase/spambase.data")
-    # dados_iris = LeitorArquivo("irisbase/iris.data")
-    # print(dados)
-    # knn = KNN(dados_iris)
-    # teste_iris = [[6.3,3.4,5.6,2.4]]
-    # teste = [0,0,1.42,0,0.71,0.35,0,0.35,0,0.71,0,0.35,0,0,0,5.35,0,0,3.21,0,2.85,0,0.35,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.102,0,0.357,0,0,1.971,24,205]
-    # print(knn.run(teste_iris,3))
-
-    dados = LeitorArquivoComTestes("spambase/spambase.data", 0.05)
+    if len(sys.argv) < 3:
+        print("Número inválido de argumentos. Abortando")
+        print("Passe o caminho do arquivo e a porcentagem para usar de teste")
+        sys.exit()
+    dados = LeitorArquivoComTestes(sys.argv[1], float(sys.argv[2]))
     knn = KNN(dados[0])
-    # print(dados[1])
     resultado_knn = knn.run(dados[1], 3)
     resultados_esperados = []
     for dado in dados[1]:
         resultados_esperados.append(dado[-1])
     
-    print(resultado_knn)
-    print(resultados_esperados)
     matrizConfusao = MatrizConfusao(dados[2])
     matrizConfusao.geraMatriz(resultado_knn, resultados_esperados)
+    print(matrizConfusao)
 
-    matConf = matrizConfusao.getMatriz()
 
-    matrizAcertos.negativoNegativo = matConf["0"]["0"]
-    matrizAcertos.negativoPositivo = matConf["0"]["1"]
-    matrizAcertos.positivoNegativo = matConf["1"]["0"]
-    matrizAcertos.positivoPositivo = matConf["1"]["1"]
-
-    print(matrizAcertos)
-
-    calculadoraEstatisticas = CalculadoraEstatisticas(matrizAcertos)
+    calculadoraEstatisticas = CalculadoraEstatisticas(matrizConfusao)
 
     print(calculadoraEstatisticas)
 
